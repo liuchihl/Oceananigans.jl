3c80b12d3a03691b725426795957b52349ca031e
Chih-Lun Liu <cliu28@dt-login04.delta.ncsa.illinois.edu>
2024-10-29 23:48:32 -0500

    add required operators in Conjugate gradient poisson solver

--- src/Solvers/conjugate_gradient_poisson_solver.jl
+++ src/Solvers/conjugate_gradient_poisson_solver.jl
@@ -1,4 +1,4 @@
-using Oceananigans.Operators: divᶜᶜᶜ, ∇²ᶜᶜᶜ 
+using Oceananigans.Operators: divᶜᶜᶜ, ∇²ᶜᶜᶜ, Ayᶜᶠᶜ, Δyᶜᶠᶜ, Vᶜᶜᶜ, Azᶜᶜᶠ, Δzᶜᶜᶠ, Δxᶠᶜᶜ, Axᶠᶜᶜ 
 using Oceananigans.ImmersedBoundaries: ImmersedBoundaryGrid
 using Statistics: mean
 
