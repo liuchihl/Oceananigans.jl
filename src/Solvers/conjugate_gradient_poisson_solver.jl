48916ec2d699269169760569a70993d35f375bfb
Chih-Lun (Aaron) Liu <chihlunl@gmail.com>
2024-11-02 14:54:28 -0700

    add Operators explicitly

--- src/Solvers/conjugate_gradient_poisson_solver.jl
+++ src/Solvers/conjugate_gradient_poisson_solver.jl
@@ -1,5 +1,4 @@
-using Oceananigans.Operators
-# divᶜᶜᶜ, ∇²ᶜᶜᶜ, Ayᶜᶠᶜ, Δyᶜᶠᶜ, Vᶜᶜᶜ, Azᶜᶜᶠ, Δzᶜᶜᶠ, Δxᶠᶜᶜ, Axᶠᶜᶜ, Δzᶜᶜᶜ, Δxᶜᶜᶜ, Δyᶜᶜᶜ
+using Oceananigans.Operators: divᶜᶜᶜ, ∇²ᶜᶜᶜ, Ayᶜᶠᶜ, Δyᶜᶠᶜ, Vᶜᶜᶜ, Azᶜᶜᶠ, Δzᶜᶜᶠ, Δxᶠᶜᶜ, Axᶠᶜᶜ, Δzᶜᶜᶜ, Δxᶜᶜᶜ, Δyᶜᶜᶜ
 using Oceananigans.ImmersedBoundaries: ImmersedBoundaryGrid
 using Statistics: mean
 
